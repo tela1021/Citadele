69fdeb5cb4cb9cf2d1aa570c78e0e7b20d8a68eb
poster <telas1020@gmail.com>
2019-09-29 18:56:25 +0300

    removed old comment

--- src/test/java/SuiteTestClass.java
+++ src/test/java/SuiteTestClass.java
@@ -1,9 +1,7 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-/**
- * Created by retinka on 13.01.2016.
- */
+
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         visualTest.class,
